be4fbaea167ae139aee53915d5658141bc81cd44
Valeriia Frolenko <valeriiiafrol@gmail.com>
2026-06-15 08:33:40 +0300

    set up the admin panel

--- travellino/users/admin.py
+++ travellino/users/admin.py
@@ -1,5 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, UserProfile
 
-admin.site.register(CustomUser)
-admin.site.register(UserProfile)
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name = 'Profile'
+    fields = ('photo', 'preferences')
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    inlines = (UserProfileInline,)
+
+    list_display = ('email', 'first_name', 'last_name', 'phone', 'is_active', 'is_staff', 'is_email_verified')
+    list_filter = ('is_active', 'is_staff', 'is_superuser', 'is_email_verified')
+    search_fields = ('email', 'first_name', 'last_name', 'phone')
+    ordering = ('email',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'phone')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'is_email_verified', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'phone', 'password1', 'password2'),
+        }),
+    )
