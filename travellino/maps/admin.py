083a24f0f6975088cac4a0c53141f9b8c121c721
Valeriia Frolenko <valeriiiafrol@gmail.com>
2026-06-15 08:51:20 +0300

    set up the admin maps

--- travellino/maps/admin.py
+++ travellino/maps/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import VisitedCountry
 
-# Register your models here.
+
+@admin.register(VisitedCountry)
+class VisitedCountryAdmin(admin.ModelAdmin):
+    list_display = ('user', 'country_name', 'country_code', 'visited_at')
+    list_filter = ('country_name',)
+    search_fields = ('user__email', 'country_name', 'country_code')
